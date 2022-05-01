b9bfb4d92fd0f30c1298ab44a0d36a1d6e58065a
neros2k <neros2k@gmail.com>
2022-05-02 01:37:52 +0300

    Создание абстракции движка.

--- src/main/java/n2k/nthirst/base/IEngine.java
+++ src/main/java/n2k/nthirst/base/IEngine.java
@@ -1,3 +1,10 @@
 package n2k.nthirst.base;
+import java.util.List;
 public interface IEngine {
+    void setWaterLevel(Float NEW_LEVEL);
+    void addActiveModifier(IModifier MODIFIER);
+    void removeModifier(IModifier MODIFIER);
+    void tick();
+    Float getWaterLevel();
+    List<IModifier> getModifierList();
 }
