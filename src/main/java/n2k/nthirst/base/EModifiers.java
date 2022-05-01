3dfe7eac8211fd7ba452777f3d25f3ba92909ad9
neros2k <neros2k@gmail.com>
2022-05-02 01:36:49 +0300

    Реализация системы модификаторов.

--- src/main/java/n2k/nthirst/base/EModifiers.java
+++ src/main/java/n2k/nthirst/base/EModifiers.java
@@ -1,3 +1,13 @@
 package n2k.nthirst.base;
 public enum EModifiers {
+    WALK((ENGINE, EVENT) -> -0.1F),
+    ON_SUN((ENGINE, EVENT) -> -0.5F),
+    IN_FIRE((ENGINE, EVENT) -> -3F);
+    private final IModifier MODIFIER;
+    EModifiers(IModifier MODIFIER) {
+        this.MODIFIER = MODIFIER;
+    }
+    public IModifier getModifier() {
+        return this.MODIFIER;
+    }
 }
--- src/main/java/n2k/nthirst/base/IModifier.java
+++ src/main/java/n2k/nthirst/base/IModifier.java
@@ -1,3 +1,5 @@
 package n2k.nthirst.base;
+import org.bukkit.event.Event;
 public interface IModifier {
+    Float getValue(IEngine ENGINE, Event EVENT);
 }
