360a7bf8803f6b52376d42f13ca9cc4e3d78c251
neros2k <neros2k@gmail.com>
2022-05-07 04:41:55 +0300

    Небольшие улучшения вида кода.

--- src/main/java/n2k_/nthirst/base/modifier/IDuration.java
+++ src/main/java/n2k_/nthirst/base/modifier/IDuration.java
@@ -1,6 +1,5 @@
 package n2k_.nthirst.base.modifier;
 import n2k_.nthirst.base.IEngine;
-
 public interface IDuration {
     Long get(IEngine ENGINE);
 }
--- src/main/java/n2k_/nthirst/base/modifier/IPermanent.java
+++ src/main/java/n2k_/nthirst/base/modifier/IPermanent.java
@@ -1,6 +1,5 @@
 package n2k_.nthirst.base.modifier;
 import n2k_.nthirst.base.IEngine;
-
 public interface IPermanent {
     Boolean get(IEngine ENGINE);
 }
